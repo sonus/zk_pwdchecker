5cf8ac8f428a435003bce064e817687f2ba7c800
Sonus Sundar <sonusmsundar@gmail.com>
2022-04-14 11:00:27 +0530

    :recycle: Code refactoring

--- src/Service/Password/PasswordDto.php
+++ src/Service/Password/PasswordDto.php
@@ -10,16 +10,10 @@ use Symfony\Component\Validator\Context\ExecutionContextInterface;
 class PasswordDto
 {
     #[Assert\NotBlank()]
+    #[Assert\Regex(['pattern' => '/\d/', 'match' => true, 'message' => 'Your password should contain a number'])]
+    #[Assert\Regex(['pattern' => '/^.{5,}$/', 'match' => true, 'message' => 'Your password should 5 characters length'])]
+    #[Assert\Regex(['pattern' => '/(.)\1\1/', 'match' => false, 'message' => 'Repeating consecutive characters are not allowed.'])]
+    #[Assert\Regex(['pattern' => '/(?=.*[A-Z]|[^a-zA-Z0-9])/', 'match' => true, 'message' => 'Atleast one uppercase or special characters in required'])]
+    #[Assert\NotCompromisedPassword()]
     public string $password;
-
-    #[Assert\Callback()]
-    public function validate(ExecutionContextInterface $context): void
-    {
-        if (isset($this->password) && !PasswordService::checkPassword($this->password)) {
-            $context
-                    ->buildViolation('The password is not valid')
-                    ->atPath('password')
-                    ->addViolation();
-        }
-    }
 }
